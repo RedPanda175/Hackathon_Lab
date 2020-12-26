c4854a2902c53bca5dfc443ee0128bf20a30ca6e
tomer99a <69591504+tomer99a@users.noreply.github.com>
2020-12-27 00:14:27 +0200

    Server started, listening on IP address

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
 
-msgFromClient = "Hello UDP Server"
-bytesToSend = str.encode(msgFromClient)
-serverAddressPort = ("127.0.0.1", 3000)
+serverAddressPort = ("127.0.0.1", 13117)
 bufferSize = 2048
+print("Client started, listening for offer requests...")
 
+msgFromClient = "0xfeedbeef"
+bytesToSend = str.encode(msgFromClient)
 clientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 clientSocket.sendto(bytesToSend, serverAddressPort)
 
 resv_message, serverAddress = clientSocket.recvfrom(bufferSize)
+print("Received offer from {} attempting to connect...".format(serverAddress[0]))
 
 clientSocket.close()
 print(resv_message)
+print(serverAddress)
--- server.py
+++ server.py
@@ -1,21 +1,26 @@
 import socket
+import threading
+
+
+def handle_client_connection():
+    print("handle_client_connection")
+
 
 bind_ip = "127.0.0.1"
-bind_port = 3000
+bind_port = 13117
 bufferSize = 2048
+max_clients = 4
+print("Server started, listening on IP address - " + bind_ip)
 
 msgFromServer = "Hello UDP Client"
 bytesToSend = str.encode(msgFromServer)
-
 serverSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 serverSocket.bind((bind_ip, bind_port))
-print("UDP server up and listening")
 
-while True:
+clients_address = []
+while len(clients_address) < max_clients:
     message, clientAddress = serverSocket.recvfrom(bufferSize)
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP = "Client IP Address:{}".format(clientAddress)
-    print(clientMsg)
-    print(clientIP)
-    serverSocket.sendto(bytesToSend, clientAddress)
-    break
+    clients_address.apped(clientAddress)
+    client_handler = threading.Thread(target=handle_client_connection)
+    client_handler.start()
+    #serverSocket.sendto(bytesToSend, clientAddress)
