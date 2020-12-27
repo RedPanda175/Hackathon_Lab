696dd9dfeef305c8e59917c59d008d933abdbe8b
tomer99a <69591504+tomer99a@users.noreply.github.com>
2020-12-27 22:09:08 +0200

    moree

--- client.py
+++ client.py
@@ -5,16 +5,23 @@ bufferSize = 2048
 print("Client started, listening for offer requests...")
 
 udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+#udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 udp_client_socket.bind(serverAddressPort)
 resv_message, serverAddress = udp_client_socket.recvfrom(bufferSize)
+#udp_client_socket.close()
 print("Received offer from {} attempting to connect...".format(serverAddress[0]))
+udp_port = serverAddress[1]
+print(udp_port)
 
-if bytes.hex(resv_message)[:8] == "feedbeef":
+resv_message = resv_message.decode("utf-8")
+#resv_message = bytes.hex(resv_message)
+if resv_message[:8] == "feedbeef":
     print("the correct password")
-    print(resv_message, serverAddress)
-    serverAddress = ('127.0.0.1', 9999)
+    serverAddress = ('127.0.0.1', int(resv_message[-4:]))
     tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_client_socket.connect(serverAddress)
-    tcp_client_socket.send(str.encode("team rokets\n"))
-    print("send message")
+    tcp_client_socket.send(str.encode("team rokets port {}\n".format(udp_port)))
+    message = tcp_client_socket.recv(bufferSize)
+    message = message.decode("utf-8")
+    print(message)
--- server.py
+++ server.py
@@ -1,23 +1,60 @@
 import socket
 import threading
 import time
+from random import shuffle
 
 
-def tcp_server(bind_port):
+all_teams = {}
+
+
+def main_tcp_funk():
     bind_ip = "127.0.0.1"
+    bind_port = 9999
 
     tcp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     tcp_server_socket.bind((bind_ip, bind_port))
     tcp_server_socket.listen(1)
-    tcp_server_socket.settimeout(15)
+    tcp_server_socket.settimeout(1)
+    i = 0
+    future = time.time() + 10
 
-    message, address = tcp_server_socket.accept()
+    while time.time() < future:
+        try:
+            connected_socket, address = tcp_server_socket.accept()
+        except:
+            continue
+        thread_funk = threading.Thread(target=recv_name, args=(connected_socket,))
+        thread_funk.start()
+        i += 1
+    all_teams_lst = list(all_teams.keys())
+    shuffle(all_teams_lst)
+    team1 = all_teams_lst[:len(all_teams_lst)//2]
+    team2 = all_teams_lst[len(all_teams_lst)//2:]
+    message_to_send_at_begin = "Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n"
+    for player_name in team1:
+        message_to_send_at_begin += player_name + "\n"
+    message_to_send_at_begin += "\nGroup 2:\n"
+    for player_name in team2:
+        message_to_send_at_begin += player_name + "\n"
+    message_to_send_at_begin += "Start pressing keys on your keyboard as fast as you can!!"
+    for player_name in all_teams_lst:
+        thread_funk = threading.Thread(target=handel_client, args=(message_to_send_at_begin, player_name))
+        thread_funk.start()
+    print(message_to_send_at_begin)
 
-    client_ip = address[0]
-    client_port = address[1]
-    print(address)
-    print("ip - " + client_ip)
-    print("port - " + str(client_port))
+
+def recv_name(connected_socket):
+    global all_teams
+    message = connected_socket.recv(2048)
+    message = message.decode("utf-8")
+    print(message)
+    all_teams[message] = connected_socket
+
+
+def handel_client(message, client_name):
+    global all_teams
+    connection = all_teams[client_name]
+    connection.send(str.encode(message))
 
 
 def udp_server():
@@ -27,29 +64,28 @@ def udp_server():
 
     udp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
     # Enable broadcasting mode
-    udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 10)
-    udp_server_socket.settimeout(10)
+    udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    udp_server_socket.settimeout(30)
     # udp_server_socket.bind((bind_ip, bind_port))
     # bytesToSend1 = bytes(0xfeedbeef)
-    bytesToSend1 = bytes.fromhex('feedbeef9999')
-    bytesToSend2 = bytes(0x2)
-    bytesToSend3 = bytes(0x9999)
+    bytes_to_send1 = str.encode('feedbeef9999')
+    bytes_to_send2 = bytes(0x2)
+    bytes_to_send3 = bytes(0x9999)
 
     count_time = 0
-    clients_address = []
     while count_time < 10:
-        udp_server_socket.sendto(bytesToSend1, (bind_ip, bind_port))
+        udp_server_socket.sendto(bytes_to_send1, (bind_ip, bind_port))
         time.sleep(1)
         count_time += 1
-        print(bind_ip, bind_port)
-        # thread_funk = threading.Thread(target=tcp_server)
-        # thread_funk.start()
-    # udp_server_socket.sendto(bytesToSend, ('<broadcast>', bind_port))
-    # print(clientAddress, bytes.hex(message))
-    # clients_address.append(clientAddress)
-    # udp_server_socket.sendto(bytesToSend, clientAddress)
+        print(count_time)
+    print("No more players for know")
 
 
 client_handler = threading.Thread(target=udp_server)
 client_handler.start()
-
+client_handler = threading.Thread(target=main_tcp_funk)
+client_handler.start()
+time.sleep(20)
+print("Players: ")
+for key in all_teams:
+    print(key)
