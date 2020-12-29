339304a613ca72208c79de616c465fd85ef9ac2b
tomer99a <69591504+tomer99a@users.noreply.github.com>
2020-12-29 15:30:48 +0200

    8473

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
-import socket
+from socket import *
 import struct
 
-print("this is it")
 server_address_port = ("127.0.0.1", 13117)
 bufferSize = 2048
 print("Client started, listening for offer requests...")
 
-udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
-# udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-udp_client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+udp_client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)  # UDP
+# udp_client_socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
+udp_client_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 udp_client_socket.bind(server_address_port)
 receive_message, server_address = udp_client_socket.recvfrom(bufferSize)
 # udp_client_socket.close()
@@ -20,15 +19,15 @@ receive_message = struct.unpack('IBH', receive_message)
 print("receive message - ", receive_message)
 magic_cookie = receive_message[0]
 message_type = receive_message[1]
-tcp_port = 9999
+tcp_port = 8473
 
 if magic_cookie == 4276993775:
     print("the correct password")
-    server_address = ('127.0.0.1', int(tcp_port))
+    server_address = ("127.0.0.1", int(tcp_port))
     print("server address - ", server_address)
-    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_client_socket = socket(AF_INET, SOCK_STREAM)
     tcp_client_socket.connect(server_address)
-    tcp_client_socket.send(str.encode("team rocket port {}\n".format(udp_port)))
+    tcp_client_socket.send(str.encode("team rocket"))
     # the problem is that we are trying to connect to the master TCP server port, and not to the address we get from this tcp server.
     # the process should be: wait for UDP -> rcv offer -> connect to master TCP -> rcv address to connect -> connect to address
     # -> send msg to address
--- server.py
+++ server.py
@@ -5,46 +5,29 @@ from random import shuffle
 import struct
 
 all_teams = {}
+time_to_connect = 13
 
 
 def tcp_main_listener():
+    global time_to_connect
     bind_ip = "127.0.0.1"
-    bind_port = 9999
+    bind_port = 8473
 
     tcp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
     tcp_server_socket.bind((bind_ip, bind_port))
-    tcp_server_socket.settimeout(30)
+    tcp_server_socket.settimeout(time_to_connect)
+    tcp_server_socket.listen(10)
     start_time = time.time()
-    while start_time + 30 > time.time():
+    while start_time + time_to_connect > time.time():
         try:
-            print("wait for some tcp connection")
             connected_socket, address = tcp_server_socket.accept()
             thread_funk = threading.Thread(target=recv_name, args=(connected_socket,))
             thread_funk.start()
-            tcp_server_socket.settimeout(time.time() - start_time)
         except:
+            if time_to_connect - time.time() + start_time > 0:
+                tcp_server_socket.settimeout(time_to_connect - time.time() + start_time)
             continue
 
-
-def main_tcp_funk():
-    bind_ip = "127.0.0.1"
-    bind_port = 9999
-
-    tcp_server_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-    tcp_server_socket.bind((bind_ip, bind_port))
-    tcp_server_socket.listen(1)
-    tcp_server_socket.settimeout(0)
-    i = 0
-    future = time.time() + 30
-
-    while time.time() < future:
-        try:
-            connected_socket, address = tcp_server_socket.accept()
-        except:
-            continue
-        thread_funk = threading.Thread(target=recv_name, args=(connected_socket,))
-        thread_funk.start()
-        i += 1
     all_teams_lst = list(all_teams.keys())
     shuffle(all_teams_lst)
     team1 = all_teams_lst[:len(all_teams_lst) // 2]
@@ -65,9 +48,9 @@ def main_tcp_funk():
 def recv_name(connected_socket):
     global all_teams
     message = connected_socket.recv(2048)
-    message = message.decode("utf-8")
+    message = message.decode("utf-8") + str(len(all_teams))
     print(message)
-    print("boo")
+    connected_socket.send(str.encode("last msg"))
     all_teams[message] = connected_socket
 
 
@@ -78,6 +61,7 @@ def handle_client(message, client_name):
 
 
 def udp_server():
+    global time_to_connect
     bind_ip = "127.0.0.1"
     bind_port = 13117
     print("Server started, listening on IP address - " + bind_ip)
@@ -85,16 +69,16 @@ def udp_server():
     # Enable broadcasting mode
     # udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     udp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    udp_server_socket.settimeout(30)
+    udp_server_socket.settimeout(time_to_connect)
     # udp_server_socket.bind((bind_ip, bind_port))
     # bytesToSend1 = bytes(0xfeedbeef)
     magic_cookie = 0xfeedbeef
     message_type = 0x2
-    server_tcp_port = 0x9999
+    server_tcp_port = 0x8473
     message = struct.pack('IBH', magic_cookie, message_type, server_tcp_port)
 
     count_time = 0
-    while count_time < 30:
+    while count_time < time_to_connect:
         udp_server_socket.sendto(message, (bind_ip, bind_port))
         time.sleep(1)
         count_time += 1
@@ -106,7 +90,7 @@ client_handler = threading.Thread(target=udp_server)
 client_handler.start()
 client_handler2 = threading.Thread(target=tcp_main_listener)
 client_handler2.start()
-time.sleep(33)
+time.sleep(time_to_connect + 3)
 print("Players: ")
 for key in all_teams:
     print(key)
