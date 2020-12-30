1fdf8b6863d2916323e663bd11a19f9d31fa5d86
RedPanda175 <goldinel@post.bgu.ac.il>
2020-12-30 17:50:38 +0200

    Update client.py

--- client.py
+++ client.py
@@ -17,6 +17,26 @@ def getch():
         return ch
     return _getch()
 
+class colors:
+    CEND      = '\33[0m'
+
+    CBLACK  = '\33[30m'
+    CRED    = '\33[31m'
+    CGREEN  = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE   = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE  = '\33[36m'
+    CWHITE  = '\33[37m'
+
+    CGREY    = '\33[90m'
+    CRED2    = '\33[91m'
+    CGREEN2  = '\33[92m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2   = '\33[94m'
+    CVIOLET2 = '\33[95m'
+    CBEIGE2  = '\33[96m'
+    CWHITE2  = '\33[97m'
 
 class Client:
     def __init__(self, ip):
@@ -25,7 +45,7 @@ class Client:
     def udp_part(self):
         server_address_port = (self.ip, 13125)
         buffer_size = 1024
-        print("Client started, listening for offer requests...")
+        print(colors.CBLUE + "Client started, listening for offer requests..." + colors.CEND)
 
         udp_client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)  # UDP
         udp_client_socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
@@ -39,7 +59,7 @@ class Client:
                 print("error")
         receive_message, server_address = udp_client_socket.recvfrom(buffer_size)
         # udp_client_socket.close()
-        print("Received offer from {} attempting to connect...".format(server_address[0]))
+        print(colors.CYELLOW + "Received offer from {} attempting to connect...".format(server_address[0] + colors.CEND))
 
         tcp_ip = server_address[0]
         receive_message = struct.unpack('IBH', receive_message)
