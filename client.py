c7a54cdbed93f8706018efd2b7463e80dd36dbf2
tomer99a <69591504+tomer99a@users.noreply.github.com>
2020-12-30 23:06:26 +0200

    client comment

--- client.py
+++ client.py
@@ -4,9 +4,12 @@ import struct
 from server import Colors
 
 
+# funk from the web to get chat from user
 def getch():
     import termios
-    import sys, tty
+    import sys
+    import tty
+
     def _getch():
         fd = sys.stdin.fileno()
         old_settings = termios.tcgetattr(fd)
@@ -23,16 +26,19 @@ def getch():
 class Client:
     def __init__(self, ip):
         self.ip = ip
+        self.buffer_size = 1024
 
     def udp_part(self):
         server_address_port = (self.ip, 13117)
-        buffer_size = 1024
         print(Colors.CBLUE + "Client started, listening for offer requests..." + Colors.CEND)
 
-        udp_client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)  # UDP
+        # init the UDP socket
+        udp_client_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         udp_client_socket.setsockopt(SOL_SOCKET, SO_REUSEPORT, 1)
         udp_client_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
         did_bind = False
+
+        # wait for some connection
         while not did_bind:
             try:
                 time.sleep(0.1)
@@ -40,15 +46,14 @@ class Client:
                 did_bind = True
             except:
                 print("error")
-        receive_message, server_address = udp_client_socket.recvfrom(buffer_size)
-        # udp_client_socket.close()
+        receive_message, server_address = udp_client_socket.recvfrom(self.buffer_size)
         print(
             Colors.CYELLOW + "Received offer from {} attempting to connect...".format(server_address[0] + Colors.CEND))
 
+        # organize recv data
         tcp_ip = server_address[0]
         receive_message = struct.unpack('IBH', receive_message)
         magic_cookie = hex(receive_message[0])
-        # message_type = hex(receive_message[1])
         tcp_port = hex(receive_message[2])
         tcp_port = int(tcp_port, 16)
 
@@ -56,9 +61,11 @@ class Client:
             self.tcp_part(tcp_port, tcp_ip)
 
     def tcp_part(self, tcp_port, tcp_ip):
+        # init the TCP socket
         server_address = (tcp_ip, int(tcp_port))
         tcp_client_socket = socket(AF_INET, SOCK_STREAM)
         did_connect = False
+        # wait for some connection
         while not did_connect:
             try:
                 time.sleep(0.1)
@@ -66,14 +73,19 @@ class Client:
                 did_connect = True
             except:
                 continue
+        # send team name
         tcp_client_socket.send(str.encode("FSM\n"))
-        message = tcp_client_socket.recv(1024)
+        message = tcp_client_socket.recv(self.buffer_size)
         message = message.decode("utf-8")
         print(message)
+
+        # start the game !!!
         future = time.time() + 10
         while future > time.time():
             tcp_client_socket.send(str.encode(getch()))
-        message = tcp_client_socket.recv(1024)
+
+        # recv end message
+        message = tcp_client_socket.recv(self.buffer_size)
         message = message.decode("utf-8")
         print(message)
         self.udp_part()
