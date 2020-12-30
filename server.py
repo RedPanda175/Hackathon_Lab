e7e641e234dea8073db0e026ec71bef7d5a2ecb4
RedPanda175 <goldinel@post.bgu.ac.il>
2020-12-30 16:54:56 +0200

    Update server.py
    start colors

--- server.py
+++ server.py
@@ -5,6 +5,41 @@ from random import shuffle
 import struct
 import operator
 
+class colors:
+    CEND      = '\33[0m'
+    CBOLD     = '\33[1m'
+    CITALIC   = '\33[3m'
+    CURL      = '\33[4m'
+    CBLINK    = '\33[5m'
+    CBLINK2   = '\33[6m'
+    CSELECTED = '\33[7m'
+
+    CBLACK  = '\33[30m'
+    CRED    = '\33[31m'
+    CGREEN  = '\33[32m'
+    CYELLOW = '\33[33m'
+    CBLUE   = '\33[34m'
+    CVIOLET = '\33[35m'
+    CBEIGE  = '\33[36m'
+    CWHITE  = '\33[37m'
+
+    CBLACKBG  = '\33[40m'
+    CREDBG    = '\33[41m'
+    CGREENBG  = '\33[42m'
+    CYELLOWBG = '\33[43m'
+    CBLUEBG   = '\33[44m'
+    CVIOLETBG = '\33[45m'
+    CBEIGEBG  = '\33[46m'
+    CWHITEBG  = '\33[47m'
+
+    CGREY    = '\33[90m'
+    CRED2    = '\33[91m'
+    CGREEN2  = '\33[92m'
+    CYELLOW2 = '\33[93m'
+    CBLUE2   = '\33[94m'
+    CVIOLET2 = '\33[95m'
+    CBEIGE2  = '\33[96m'
+    CWHITE2  = '\33[97m'
 
 class Server:
     def __init__(self, ip):
@@ -34,7 +69,7 @@ class Server:
         while start_time + self.time_to_connect > time.time():
             try:
                 connected_socket, address = tcp_server_socket.accept()
-                print(address)
+                print(colors.CYELLOW+address)
                 thread_funk = threading.Thread(target=self.handle_client, args=(connected_socket, condition))
                 thread_funk.start()
             except:
